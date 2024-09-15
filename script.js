9c534f35093ca7a5066eed5aa7e8bfaa503b0c8f
farhan <ff1917914@gmail.com>
2024-09-15 12:40:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,211 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Q1//////////////////////
+
+// var city = prompt("Enter your city name");
+
+
+
+
+// if(city === "karachi"){
+
+// alert("welcome to city of lights")
+
+// }
+
+
+// Q2//////////////////////
+
+
+// var gander = prompt("Enter your gander");
+
+// if(gander === "male"){
+
+// alert("good morning sir")
+
+// }
+
+// if(gander === "female"){
+
+//     alert("good morning ma'am")
+
+// }
+
+
+// Q3//////////////
+
+// var trafic = prompt("Enter your signal");
+// var  yellow = "yellow"
+// var green = "green"
+
+
+
+
+
+
+
+// if(trafic === "green"){
+
+//     document.write(`<div class="con1"><h3>signal</h3> &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   <h3>message</h3></div>
+    
+//     <div class="con2"><h3>red</h3>
+//      &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+    
+//     <h3></h3></div>
+    
+//     <div class="con3"><h3>yellow</h3>
+//      &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+    
+//     <h3></h3></div>
+    
+//     <div class="con4"><h3>green</h3>
+//      &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+    
+//     <h3>${green}</h3></div>
+    
+//     `)
+    
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(trafic === "yellow"){
+
+//     document.write(`<div class="con1"><h3>signal</h3> &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   <h3>message</h3></div>
+    
+//     <div class="con2"><h3>red</h3>
+//      &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+    
+//     <h3></h3></div>
+    
+//     <div class="con3"><h3>yellow</h3>
+//      &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+    
+//     <h3>${yellow}</h3></div>
+    
+//     <div class="con4"><h3>green</h3>
+//      &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+    
+//     <h3></h3></div>
+    
+//     `)
+    
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(trafic === "red"){
+
+// document.write(`<div class="con1"><h3>signal</h3> &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   <h3>message</h3></div>
+
+// <div class="con2"><h3>red</h3>
+//  &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+
+// <h3>${trafic}</h3></div>
+
+// <div class="con3"><h3>yellow</h3>
+//  &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+
+// <h3></h3></div>
+
+// <div class="con4"><h3>green</h3>
+//  &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+
+// <h3></h3></div>
+
+// `)
+
+// }
+
+
+// Q4
+
+
+// chapter no 10////////////////
+
+// Q1
+
+
+// var city = prompt("enter your city name");
+
+// if(city === "karachi"){
+
+// console.log("the city of lights")
+
+// }
+
+
+// Q2
+
+
+// var num= +prompt("enter you number");
+// var y = 10
+// var x = 20 
+//  if( x === y){
+//     alert("heyy you are succecfull")
+
+//  }
+
+// Q3
+
+// var num = +prompt("enter you city name");
+// var city = 'karachi'
+
+// if(num === "10010"){
+// alert("welcome boy")
+// }
+
+
+
+
